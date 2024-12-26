f086773af1934f5fa4dfaf7c35f7912b6ee85986
Eslam Amin <eslam.amin.610@gmail.com>
2024-12-26 19:00:13 +0200

    Added typing, stopTyping SocketEvents

--- index.js
+++ index.js
@@ -1,79 +1,99 @@
-const express = require("express")
+const express = require("express");
 const http = require("http");
-const app = express()
+const app = express();
 const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
-    cors: {
-        origin: "*", // Allow connections from any origin
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "*", // Allow connections from any origin
+    methods: ["GET", "POST"]
+  }
 });
 const PORT = process.env.PORT || 4000;
-
 
 let users = [];
 
 const addUser = (user, socketId) => {
-    // console.log("------Test Add user----")
-    // console.log(user)
-    if (!users.some(currentUser => currentUser.user._id === user._id)) {
+  // console.log("------Test Add user----")
+  // console.log(user)
+  if (!users.some((currentUser) => currentUser.user._id === user._id)) {
+    users.push({ user, socketId });
+  }
 
-        users.push({ user, socketId })
-
-    }
-
-    // console.log(users)
-}
+  // console.log(users)
+};
 const removeUser = (socketId) => {
-    users = users.filter(user => user.socketId !== socketId);
-}
+  users = users.filter((user) => user.socketId !== socketId);
+};
 
 const getUser = (userId) => {
-    // console.log("-----Test Get User------")
-    // console.log(users)
-    return users.find(user => user.user._id === userId);
-}
+  // console.log("-----Test Get User------")
+  // console.log(users)
+  return users.find((user) => user.user._id === userId);
+};
 
 io.on("connection", (socket) => {
-    //when connect
-    console.log("a user connected.")
+  //when connect
+  console.log("a user connected.");
 
-    //take userId and socketId from user
-    socket.on("addUser", user => {
-        addUser(user, socket.id);
-        io.emit("getOnlineUsers", users)
-    });
-    socket.on("removeUser", user => {
-        removeUser(user, socket.id);
-        io.emit("getOnlineUsers", users)
-    });
+  //take userId and socketId from user
+  socket.on("addUser", (user) => {
+    addUser(user, socket.id);
+    io.emit("getOnlineUsers", users);
+  });
+  socket.on("removeUser", (user) => {
+    removeUser(user, socket.id);
+    io.emit("getOnlineUsers", users);
+  });
 
+  //send and get message
+  socket.on("sendMessage", ({ sender, receiverId, text }) => {
+    const user = getUser(receiverId);
+    // console.log(receiverId, user)
+    if (user)
+      io.to(user.socketId).emit("getMessage", {
+        senderId: sender._id,
+        profilePicture: sender.profilePicture,
+        gender: sender.gender,
+        isAdmin: sender.isAdmin,
+        text
+      });
+  });
 
-    //send and get message
-    socket.on("sendMessage", ({ sender, receiverId, text }) => {
-        const user = getUser(receiverId);
-        // console.log(receiverId, user)
-        if (user)
-            io.to(user.socketId).emit("getMessage", {
-                senderId: sender._id,
-                profilePicture: sender.profilePicture,
-                gender: sender.gender,
-                isAdmin: sender.isAdmin,
-                text
-            });
-    })
+  socket.on("typing", ({ sender, receiverId }) => {
+    const user = getUser(receiverId);
+    if (user) {
+      io.to(user.socketId).emit("typing", {
+        senderId: sender._id,
+        senderName: sender.name,
+        receiverId
+      });
+    }
+  });
 
-    socket.on("disconnect", () => {
-        //when disconnect
-        console.log("a user disconnected")
-        io.emit("getOnlineUsers", users)
-        removeUser(socket.id)
-    })
-})
+  socket.on("stopTyping", ({ sender, receiverId }) => {
+    const user = getUser(receiverId);
+    if (user) {
+      io.to(user.socketId).emit("stopTyping", {
+        senderId: sender._id,
+        senderName: sender.name,
+        receiverId
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    //when disconnect
+    console.log("a user disconnected");
+    io.emit("getOnlineUsers", users);
+    removeUser(socket.id);
+  });
+});
 
 app.get("/", (req, res) => {
-    res.send("socket.io server is running");
-})
+  res.send("socket.io server is running");
+});
 
-server.listen(PORT, () => console.log("Server is up & running!"))
+server.listen(PORT, () =>
+  console.log(`Server is up on PORT ${PORT} & running!`)
+);
